9c6f74524a0143131ee8e28598b8e451f1a47b2b
Imjustheretobehere <jackson.dennis2208@gmail.com>
2025-11-08 16:16:55 -0500

    install dependencies shell script
    wowzas

---
+++ todolist/installdependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Dependencies : 0/2"
+echo "Installing ujson..."
+pip3 install ujson && echo "Dependencies : 1/2"
+echo "Installing rich..."
+pip3 install rich && echo "Dependencies : 2/2"
