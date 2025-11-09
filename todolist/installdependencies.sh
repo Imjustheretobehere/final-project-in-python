bcf929be870a117b61d1c3a9ecaf94aaf48aed1f
Jackson Dennis <jackson.dennis2208@gmail.com>
2025-11-09 14:57:42 -0500

    deleted ujson from installer
    i swear if i need to add back ujson-

--- todolist/installdependencies.sh
+++ todolist/installdependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Dependencies : 0/2"
-echo "Installing ujson..."
-pip3 install ujson && echo "Dependencies : 1/2"
+echo "Dependencies : 0/1"
 echo "Installing rich..."
-pip3 install rich && echo "Dependencies : 2/2"
+pip3 install rich && echo "Dependencies : 1/1"
