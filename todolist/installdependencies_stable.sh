89d317270c6af1f3b1a909b8f855c271e1d851bf
Jackson Dennis <jackson.dennis2208@gmail.com>
2025-11-09 15:06:06 -0500

    deleted both

--- todolist/installdependencies_stable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Dependencies : 0/2"
-echo "Installing rich..." && pip3 install rich
-echo "Dependencies : 1/2"
-echo "Installing ujson..." && pip3 install ujson
-echo "Dependencies : 2/2"
-sleep 3
